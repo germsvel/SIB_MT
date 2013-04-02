fe5d7fc6ee4cef70ce1eb7a58a98eba83758acb9
German Velasco <germsvel@gmail.com>
2013-04-02 19:25:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SIBMt::Application.config.secret_token = '9636eed13f559f54363dc36b5abc47c22349214fd84250458489d59c78a116f30cee9dd0c36bc9d0758f63f66c7e08f6cae264bcdcf87d6b7dff2f41a0355498'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+SIBMt::Application.routes.draw do
+  root :to => 'profiles#index'
+
+  resources :profiles, :only => [:index, :show]
+end
