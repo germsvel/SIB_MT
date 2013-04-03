801e6636a6485294e5447c92e69ffb9f17c3ceaa
German Velasco <germsvel@gmail.com>
2013-04-02 20:55:51 -0400

    ran migrations, created show page, and created three profiles for cmo, social strategist, and creative agency

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,14 @@
 class ProfilesController < ApplicationController
 
 layout 'home', :only => :index 
+
+
+def index
+  @profiles = Profile.all
+end
+
+def show
+  @profile = Profile.find(params[:id])
+end
+
 end
