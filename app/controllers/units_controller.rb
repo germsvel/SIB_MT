a553770f4a2c62f8de930f7ec8ac021d5cff3e87
German Velasco <germsvel@gmail.com>
2013-04-04 19:02:16 -0400

    Created Input forms for units and created FAQ model and some partials for the main profile pages

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -4,8 +4,31 @@ class UnitsController < ApplicationController
     @units = Unit.all
   end
 
+  def new
+    @unit = Unit.new
+  end
+
+  def create
+    @unit = Unit.new(params[:unit])
+
+    if @unit.save 
+      flash[:notice] = "You have successfully created a new unit"
+      redirect_to units_path
+    else
+      render 'new'
+    end
+
+  end
+
   def show
     @unit = Unit.find(params[:id])
+  end
+
+  def destroy
+    @unit = Unit.find(params[:id])
+    @unit.destroy
+    flash[:notice] = "You have successfully deleted a unit"
+    redirect_to units_path
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ SIBMt::Application.routes.draw do
 
   resources :profiles, :only => [:index, :show]
 
-  resources :units, :only => [:index, :show]
+  resources :units, :only => [:index, :show, :new, :create, :destroy]
+
+  resources :faqs, :only => [:index, :show, :new, :create, :destroy]
 
 end
