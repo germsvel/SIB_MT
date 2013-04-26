c07b6c52461544db1bdacefd0e7de20ffbaed03c
German Velasco <germsvel@gmail.com>
2013-04-26 00:20:46 -0400

    Add options to manually precomplie assets for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module SIBMt
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku precompiling option
+    config.assets.initialize_on_precompile=false
   end
 end
